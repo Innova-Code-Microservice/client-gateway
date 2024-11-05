68974711089f0391510912af5bed5e49e08075be
Ale Duarte <duartemattias4@gmail.com>
2024-11-04 21:58:29 -0400

    configuracion y validacion de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
 
@@ -14,8 +15,8 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
 
-  logger.log(`Gateway running on port 3000`)
+  logger.log(`Gateway running on port ${ envs.port }`);
 }
 bootstrap();

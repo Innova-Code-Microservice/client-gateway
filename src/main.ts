9ab315d7b0f770594bb34c097ef5a76fe42905e4
Ale Duarte <duartemattias4@gmail.com>
2024-11-04 21:41:53 -0400

    configuracion inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
+  const logger = new Logger("Main Gateway");
+
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log(`Gateway running on port 3000`)
 }
 bootstrap();
